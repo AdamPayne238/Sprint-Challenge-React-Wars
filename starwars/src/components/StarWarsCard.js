47facdeed71ebdf3d74389385aff36d9aca1f48e
Adam Payne <ajpayne238@gmail.com>
2019-08-16 16:46:41 -0400

    yerd

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './StarWars.css';
-import { Button } from 'semantic-ui-react'
-import { Header } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
+import { Card, Icon, Image } from 'semantic-ui-react';
 
 
 // function StarWarsCard(props){
@@ -31,14 +32,30 @@ import { Header } from 'semantic-ui-react'
 
 // export default StarWarsCard;
 
-const StarWarsCard = (props) => (
-    <div>
-      <Header as='h1'>{props.name}</Header>
-      <Header as='h2'>{props.gender}</Header>
-      <Header as='h3'>{props.age}</Header>
-      <Header as='h4'>{props.height}</Header>
-      <Header as='h5'>{props.mass}</Header>
-    </div>
-  )
+// const StarWarsCard = (props) => (
+//     <div>
+//       <Header as='h1'>{props.name}</Header>
+//       <Header as='h2'>{props.gender}</Header>
+//       <Header as='h3'>{props.age}</Header>
+//       <Header as='h4'>{props.height}</Header>
+//       <Header as='h5'>{props.mass}</Header>
+//     </div>
+//   )
   
-  export default StarWarsCard;
+//   export default StarWarsCard;
+
+
+
+const StarWarsCard = (props) => (
+  <Card>
+    <Card.Content>
+         <Header as='h1'>{props.name}</Header>
+  <Header as='h2'>{props.gender}</Header>
+  <Header as='h3'>{props.age}</Header>
+  <Header as='h4'>{props.height}</Header>
+  <Header as='h5'>{props.mass}</Header>
+    </Card.Content>
+  </Card>
+)
+
+export default StarWarsCard;
